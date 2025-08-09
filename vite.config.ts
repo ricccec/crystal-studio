84c4916a133407d83bfa98501597bfea861c9308
Riccardo Cecchi <65662894+ricccec@users.noreply.github.com>
2025-08-09 23:17:08 +0200

    feat: restructure project for Electron + VIte + React integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import electron from 'vite-plugin-electron/simple';
+import path from 'node:path';
 
 export default defineConfig({
+	root: path.join(__dirname, 'src/renderer'),
+	 publicDir: path.join(__dirname, 'public'),
+	build: {
+		outDir: path.join(__dirname, 'dist/renderer'),
+		emptyOutDir: true, // Clear folder before build
+	},
+	resolve: {
+		alias: {
+			'@': path.join(__dirname, 'src/renderer'),
+			'@shared': path.join(__dirname, 'src/shared'),
+		},
+	},
 	plugins: [
 		react(),
 		electron({
-			main: {
-				entry: 'electron/main.ts',
+			main: { 
+				entry: path.join(__dirname, 'src/main/main.ts'),
+				vite: {
+					build: {
+						outDir: path.join(__dirname, 'dist-electron')
+					}
+				}
 			},
 			preload: {
-				input: 'electron/preload.ts',
-			}
+				input: path.join(__dirname, 'src/preload/preload.ts'),
+				vite: {
+					build: {
+						outDir: path.join(__dirname, 'dist-electron')
+					}
+				}
+			 },
 		}),
 	],
-	build: {
-		outDir: 'dist',
-	},
 });
