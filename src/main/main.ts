4477f5049ee39c779205816b98dbb52bcb9f902f
ricccec <ricccec@LAPTOP-T5S6VRC7.localdomain>
2025-08-15 10:28:32 +0200

    feat: add ipc handlers to save project as

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,5 +1,11 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import {
+        app, dialog,
+        BrowserWindow,
+        ipcMain
+    } from 'electron';
+import type { ProcessResult } from '@shared/types/types';
 import { fileURLToPath } from 'node:url';
+import fs from 'node:fs/promises';
 import path, { dirname } from 'path';
 import type { ProjectSettings } from './utils/settings';
 import { readSettings, writeSettings } from './utils/settings';
@@ -30,7 +36,6 @@ const PROD_PUBLIC = path.join(APP_ROOT, 'dist', 'renderer');
 const VITE_PUBLIC = IS_DEV ? DEV_PUBLIC : PROD_PUBLIC;
 
 let win: BrowserWindow | null;
-let projectPath : string | null = null;
 const projectSettings : ProjectSettings = {};
 
 const createWindow = () => {
@@ -55,14 +60,128 @@ app.on('window-all-closed', () => {
     if (process.platform != 'darwin') app.quit();
 });
 
-ipcMain.handle('update-settings', (evt, newSettings : ProjectSettings) => {
-              Object.assign(projectSettings, newSettings); 
-    }
-);
 
-ipcMain.handle('save-project', async () => {
-    if (!projectPath) {
-        return { ok: false, error: 'Project path is missing' };
+const openSaveDialog = async (options?: Electron.SaveDialogOptions): Promise<ProcessResult> => {
+    if (!win) return { status: 'canceled' };
+    
+    try {
+        const { canceled, filePath } = await dialog.showSaveDialog(win, options ?? {});
+        if (canceled || !filePath) return { status: 'canceled' };
+        return { status: 'success', data: filePath };
+    } catch (e: any) {
+        return { status: 'error', error: e?.message ?? String(e) };
     }
-    return writeSettings(projectSettings, projectPath);
+};
+
+const saveProject = async (newPath?: string | null) : Promise<ProcessResult> => {
+
+    if (!win) return { status: 'canceled' };
+
+    // Cache prev name, path and tempName in case writing goes wrong
+    const oldName = projectSettings.projectName;
+    const oldPath = projectSettings.projectPath;
+    const oldTempName = projectSettings.tempName;
+    if (newPath) {
+        projectSettings.projectPath = newPath;
+        projectSettings.projectName = path.parse(newPath).name;
+    }
+    if (!projectSettings.projectPath) {
+        return { status: 'error', error: "Path not set"};
+    }
+
+    if (oldPath && (oldPath != projectSettings.projectPath)) {
+        // We are actually saving a copy
+        projectSettings.tempName = null;
+    }
+
+    try {
+        const result = await writeSettings(projectSettings);
+        if (result.ok) {
+            return { status: 'success', data: projectSettings.projectPath! }
+        } else {
+            // Restore cached props
+            projectSettings.projectName = oldName;
+            projectSettings.projectPath = oldPath;
+            projectSettings.tempName = oldTempName;
+
+            return { status: 'error', error: result.error };
+        }
+    } catch (e: any) {
+        // Restore cached props
+        projectSettings.projectName = oldName;
+        projectSettings.projectPath = oldPath;
+        projectSettings.tempName = oldTempName;
+        return { status: 'error', error: e?.message ?? String(e) };
+    }
+
+};
+
+const saveProjectForRecovery = async () : Promise<{ ok: boolean, error?: string }> => {
+
+    // Use the project temp name or generate one on the fly
+    const filename = projectSettings.tempName ?? `proj_${Date.now()}`;
+    projectSettings.tempName = filename;
+
+    const targetPath = path.join(app.getPath('userData'), `${filename}.json`);
+    
+    try {
+        // Read last saved settings, if any
+        const curr = await readSettings(targetPath);
+        
+        // Ensure the directory exists
+        await fs.mkdir(path.dirname(targetPath), { recursive: true });
+                
+        // Write project to file
+        await fs.writeFile(
+            targetPath,
+            JSON.stringify({...curr, ...projectSettings}, null, 2),
+            'utf-8'
+        );
+    
+        return { ok: true };
+    } catch (e: any) {
+        return { ok: false, error: e?.message ?? String(e) };
+    } 
+
+};
+
+ipcMain.handle('update-settings', (evt, newSettings : ProjectSettings) => {
+    Object.assign(projectSettings, newSettings); 
+});
+
+ipcMain.handle('open-save-dialog', async (_, options?: Electron.SaveDialogOptions) : Promise<ProcessResult> => {
+    return await openSaveDialog(options);
+});
+
+ipcMain.handle('open-save-project-dialog', async () : Promise<ProcessResult> => {
+    return await openSaveDialog({
+        title: 'Save project',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'JSON files', extensions: ['json'] },
+            { name: 'All Files', extensions: ['*'] },
+        ],
+    });
+});
+
+ipcMain.handle('save-project', async () : Promise<ProcessResult> => {
+    let savePath = null;
+    if (!projectSettings.projectPath) {
+        const result = await openSaveDialog({
+            title: 'Save project',
+            defaultPath: app.getPath('documents'),
+            filters: [
+                { name: 'JSON files', extensions: ['json'] },
+                { name: 'All Files', extensions: ['*'] },
+            ],
+        });
+
+        if (result.status !== 'success') return result;
+        savePath = result.data;
+    }
+    return await saveProject(savePath);
+});
+
+ipcMain.handle('save-project-as', async (_, path: string) : Promise<ProcessResult> => {
+    return await saveProject(path);
 });
--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
     ReactDOM.createRoot(rootElement).render(
         <React.StrictMode>
