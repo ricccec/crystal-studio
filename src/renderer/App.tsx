a482d57a9e5a57828838d8557468a04ff470f778
ricccec <ricccec@LAPTOP-T5S6VRC7.localdomain>
2025-08-15 11:35:41 +0200

    feat: add ipc handler to open a new project

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,4 @@
-import { ProcessResult } from '@shared/types/types';
-import React from 'react';
+import { ProcessResult } from '@shared/types/types';import React from 'react';
 
 const App = () => {
 
@@ -7,23 +6,30 @@ const App = () => {
 
     const appendToConsoleOutput = (
         result : 
-            | ProcessResult
+            | ProcessResult<any>
     ) => {
         const output = (() => {
             switch(result.status) {
                 case 'canceled': return 'canceled';
                 case 'error': return result.error;
-                case 'success': return result.data;
-        
+                case 'success': 
+                    if (result.data === undefined) return '';
+                    if (typeof result.data === 'object') return JSON.stringify(result.data, null, 2);
+                    return String(result.data);
             }
         })();
         setConsoleState(prev => `${prev}${output}\n`);
-    }
+    };
+
+    const onOpenProject = async () => {
+        const result = await window.api.openProject();
+        appendToConsoleOutput(result);
+    };
 
     const onSaveProject = async () => {
         const result = await window.api.saveProject();
         appendToConsoleOutput(result);
-    }
+    };
 
     const onSaveProjectAs = async () => {
         let result = await window.api.openSaveProjectDialog();
@@ -31,16 +37,18 @@ const App = () => {
             result = await window.api.saveProjectAs(result.data);
         }
             appendToConsoleOutput(result);
-    }
+    };
 
     return (
         <>
             <div>
+                <button onClick={onOpenProject}>Open Project</button>
                 <button onClick={onSaveProject}>Save Project</button>
                 <button onClick={onSaveProjectAs}>Save Project As</button>
             </div>
             <div>
                 <textarea
+                    rows={25}
                     style={{ width: '100%' }}
                     value={consoleOutput}
                     readOnly
--- src/shared/types/types.d.ts
+++ src/shared/types/types.d.ts
@@ -2,3 +2,17 @@ export type ProcessResult<T = string> =
     | { status: 'canceled' }
     | { status: 'success'; data: T }
     | { status: 'error'; error: string };
+
+export type ActionResul<T = null> = 
+    | { ok : true, data?: T }
+    | { ok : false, error : string };
+
+export interface ProjectSettings {
+    projectName?: string | null;
+    projectPath?: string | null;
+    repoPath?: string | null;
+    makePath?: string | null;
+    rgbdsPath?: string | null;
+    emulatorPath?: string | null;
+    tempName?: string | null;
+}
