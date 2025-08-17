494bcbe9c45c02333407533dfc671f3bc8b5b9ab
ricccec <ricccec@LAPTOP-T5S6VRC7.localdomain>
2025-08-17 12:15:59 +0200

    feat: update preload script and globals to handle the new  git IPCs

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,14 +1,26 @@
-import type { ActionResult, ProcessResult, ProjectSettings} from '@shared/types/types';
+import type {
+    ActionResult,
+    ProcessResult,
+    ProjectSettings,
+    SpawnResult
+ } from '@shared/types/types';
 import { contextBridge, ipcRenderer } from 'electron';
 
 
 contextBridge.exposeInMainWorld('api', {
     platform: process.platform,
 
+    // Project lifecycle IPCs
     newProject : (): Promise<ActionResult> => ipcRenderer.invoke('new-project'),
     saveProject : (): Promise<ProcessResult> => ipcRenderer.invoke('save-project'),
     saveProjectAs : (savePath: string): Promise<ProcessResult> => ipcRenderer.invoke('save-project-as', savePath),
     openProject : (): Promise<ProcessResult<ProjectSettings>> => ipcRenderer.invoke('open-project'),
-    openSaveDialog : (options?: Electron.SaveDialogOptions): Promise<ProcessResult> => ipcRenderer.invoke('open-save-dialog', options),
-    openSaveProjectDialog : (): Promise<ProcessResult> => ipcRenderer.invoke('open-save-project-dialog'),
+    
+    // Dialog IPCs
+    showSaveDialog : (options?: Electron.SaveDialogOptions): Promise<ProcessResult> => ipcRenderer.invoke('show-save-dialog', options),
+    showSaveProjectDialog : (): Promise<ProcessResult> => ipcRenderer.invoke('show-save-project-dialog'),
+
+    // Git IPCs
+    openGitRepo: (repoPath: string):  Promise<ActionResult> => ipcRenderer.invoke('git-open-repo', repoPath),
+    cloneGitRepo: (repoUrl: string, targetPath: string): Promise<SpawnResult> => ipcRenderer.invoke('git-clone', repoUrl, targetPath),
 });
